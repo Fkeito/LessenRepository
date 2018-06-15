ab3336c04aa14cbfa71748088d43d517e35c525e
Fkeito <>
2018-06-16 00:27:31 +0900

    RayとEnumの回

---
+++ RayEnumTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rot = new Vector3(Mathf.Clamp((-(Input.mousePosition.y-Screen.height/2))/2+20, -15, 60), Mathf.Clamp((Input.mousePosition.x- Screen.width / 2)/2, -65, 65), 0);
+        this.transform.rotation = Quaternion.Euler(rot);
+	}
+}
